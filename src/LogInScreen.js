b1f6e529590967ef16f0cbb625d666307d68e542
Richmond Baltazar <baltazarrichmond31@gmail.com>
2024-02-27 13:31:11 +0800

    Navigation
    added navigation, safeareaview

---
+++ src/LogInScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const LogInScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>SexTiktok</Text>
+    </SafeAreaView>
+  );
+};
+
+export default LogInScreen;
+
+const styles = StyleSheet.create({});
