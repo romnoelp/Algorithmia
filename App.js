66f3fb960ca09cbcfb3566e3f02b3ce4337863f9
Romnoel Petracorta <petracorta.rmnl@gmail.com>
2024-02-28 11:38:10 +0800

    Created AppearanceScreen.js

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LandingScreen from "./src/screens/LandingScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import LogInScreen from "./src/screens/LogInScreen";
+import AppearanceScreen from "./src/screens/AppearanceScreen";
+
 
 const Stack = createNativeStackNavigator();
 const App = () => (
   <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen
-        name="LandingScreen"
-        component={LandingScreen}
+        name="AppearanceScreen"
+        component={AppearanceScreen}
         options={{ headerShown: false }}
       />
       <Stack.Screen
