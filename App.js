7dd872422ba46aaebb3bb8aa1231089dbbfd2d3e
Richmond Baltazar <baltazarrichmond31@gmail.com>
2024-02-28 17:39:51 +0800

    export loadFont, SVGs, header fix

--- App.js
+++ App.js
@@ -1,34 +1,18 @@
-<<<<<<< Updated upstream
-import { StyleSheet } from "react-native";
-=======
->>>>>>> Stashed changes
 import React from "react";
 import { View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LandingScreen from "./src/LandingScreen";
 import { NavigationContainer } from "@react-navigation/native";
-<<<<<<< Updated upstream
-import LogInScreen from "./src/LogInScreen";
-=======
 import LogInScreen from "./src/screens/LogInScreen";
 import AppearanceScreen from "./src/screens/AppearanceScreen";
 import MainMenuScreen from "./src/screens/MainMenuScreen";
 import { homeSVG } from "./loadFontSVG";
 import { SvgXml } from "react-native-svg";
 import { TouchableOpacity } from "react-native";
->>>>>>> Stashed changes
 
 const Stack = createNativeStackNavigator();
 const App = () => (
   <NavigationContainer>
-<<<<<<< Updated upstream
-    <Stack.Navigator>
-      <Stack.Screen
-        name="LandingScreen"
-        component={LandingScreen}
-        options={{ headerShown: false }}
-      />
-=======
     <Stack.Navigator initialRouteName="AppearanceScreen">
       <Stack.Screen
         name="AppearanceScreen"
@@ -60,7 +44,6 @@ const App = () => (
         component={LandingScreen}
         options={{ headerShown: false }}
       />
->>>>>>> Stashed changes
       <Stack.Screen
         name="LogInScreen"
         component={LogInScreen}
