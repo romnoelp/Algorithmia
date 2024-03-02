1c0daff4ae78c6ef0b462fc54d7a32603c62be20
Romnoel Petracorta <petracorta.rmnl@gmail.com>
2024-03-02 18:57:36 +0800

    Made langind page responsive

--- src/screens/LandingScreen.js
+++ src/screens/LandingScreen.js
@@ -3,9 +3,13 @@ import { StyleSheet, Text, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import React, { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { SvgXml } from "react-native-svg";
 import { TouchableOpacity } from "react-native";
 import { loadFont, SVGLogo } from "../../loadFontSVG";
+import { SvgXml } from "react-native-svg";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const LandingScreen = ({ navigation }) => {
   const [fontLoaded, setFontLoaded] = useState(false);
@@ -24,67 +28,32 @@ const LandingScreen = ({ navigation }) => {
         colors={["#2CC5EF", "#147691", "#061215"]}
         style={styles.container}
       >
-        <View style={styles.content}>
-          <Text
-            style={[
-              styles.karmaBold,
-              { fontSize: 45, padding: 30, marginBottom: 50, marginTop: -40 },
-            ]}
-          >
-            Algorithmia
-          </Text>
+        <Text style={styles.title}>Algorithmia</Text>
+        <SvgXml xml={SVGLogo} style={styles.logo} />
+        <Text style={styles.second}>
+          Discover your inner analytics with {"\n"}Algorithmia
+        </Text>
+        <Text style={styles.third}>
+          Knapsack, Selection Sorting, TSP, and String {"\n"} Matching - Your
+          playground awaits!
+        </Text>
 
-          <View style={{ padding: 10, marginBottom: 30, marginTop: -30 }}>
-            <SvgXml xml={SVGLogo} width="180" height="150" />
-          </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            navigation.replace("TabToStack");
+          }}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
 
-          <Text
-            style={[
-              styles.karmaSemibold,
-              { fontSize: 25, paddingTop: 20, marginBottom: 40 },
-            ]}
-          >
-            Discover your inner analytics with {"\n"}Algorithmia
-          </Text>
-
-          <Text style={[styles.karmaLight, { fontSize: 20, marginBottom: 40 }]}>
-            Knapsack, Selection Sorting, TSP, and String {"\n"}
-            Matching - Your playground awaits!
-          </Text>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              navigation.replace("TabToStack"); //change to LogInScreen later
-            }}
-          >
-            <Text style={styles.buttonText}>Login</Text>
-          </TouchableOpacity>
-
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              marginBottom: 10,
-              marginTop: 10,
-            }}
-          >
-            <Text style={[styles.karmaLight, { fontSize: 15 }]}>
-              Don't have an account yet?{" "}
+        <View style={styles.signup}>
+          <Text style={[styles.nonTouchable]}>Don't have an account yet? </Text>
+          <TouchableOpacity onPress={() => {}}>
+            <Text style={styles.touchable}>
+              Sign up here!
             </Text>
-            <TouchableOpacity onPress={() => {}}>
-              <Text
-                style={{
-                  color: "#8CD4E8",
-                  fontFamily: "karma-light",
-                  marginLeft: 5,
-                  fontSize: 15,
-                }}
-              >
-                Sign up here!
-              </Text>
-            </TouchableOpacity>
-          </View>
+          </TouchableOpacity>
         </View>
       </LinearGradient>
     </SafeAreaView>
@@ -94,44 +63,63 @@ const LandingScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  content: {
-    flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    marginTop: hp("-4%"),
   },
-  karmaRegular: {
-    fontFamily: "karma-regular",
-    color: "#E4F2F6",
-    textAlign: "center",
-  },
-  karmaLight: {
-    fontFamily: "karma-light",
-    color: "#E4F2F6",
-    textAlign: "center",
-  },
-  karmaBold: {
+  title: {
     fontFamily: "karma-bold",
-    color: "#E4F2F6",
-    textAlign: "center",
+    fontSize: hp("4.5%"),
+    color: "#EBF7F9",
   },
-  karmaSemibold: {
+  second: {
     fontFamily: "karma-semibold",
-    color: "#E4F2F6",
+    color: "#EBF7F9",
+    fontSize: hp("2.5%"),
     textAlign: "center",
+    marginHorizontal: wp("7%"),
+    marginTop: hp("3%"),
+    marginBottom: hp("2%"),
+  },
+  third: {
+    fontFamily: "karma-regular",
+    color: "#EBF7F9",
+    fontSize: hp("1.9%"),
+    textAlign: "center",
+    marginHorizontal: wp("7%"),
+  },
+  logo: {
+    margin: hp("2%"),
+    height: "10%", 
+    width: "20%", 
   },
   button: {
     backgroundColor: "#147691",
-    paddingHorizontal: 35,
-    paddingVertical: 15,
+    paddingHorizontal: hp("5%"),
+    paddingVertical: hp("2%"),
     borderRadius: 20,
-    marginBottom: 50,
+    marginTop: hp("5%"),
   },
   buttonText: {
     color: "#E6F2F6",
-    fontSize: 20,
+    fontSize: hp("2.5%"),
     fontFamily: "karma-semibold",
     textAlign: "center",
+  },
+  signup: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: hp("8%"),
+  },
+  nonTouchable: {
+    fontFamily: "karma-light",
+    color: "#EBF7F9",
+    fontSize: hp("1.8%"),
+  },
+  touchable: {
+    fontFamily: "karma-light",
+    color: "#6FD1EB",
+    fontSize: hp("1.8%"),
   },
 });
 
